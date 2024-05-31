29fe9073fdaaf903348206070ef658e38af392a4
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-01 00:06:44 +0200

    clerk auth marketfing page and header redirect

--- app/(marketing)/Header.tsx
+++ app/(marketing)/Header.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import React from 'react';
 
 const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-        Header
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src="/mascot.svg"
+            alt="mascot"
+            width={40}
+            height={40}
+          />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">Lingo</h1>
+        </div>
+
+        <ClerkLoading>
+          <Loader className="size-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="redirect" forceRedirectUrl="/learn">
+              <Button size="lg" variant="ghost">Sign in</Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
