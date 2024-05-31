618d653478aee270f70cc4fd633dd4148208c3a0
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-05-31 14:32:48 +0200

    marketing skeleton

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,22 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+
+type Props = {
+    chidlren: React.ReactNode;
+}
+
+const MarketingLayout = ({ children }: Props) => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1 flex flex-col items-center justify-center">
+            {children}
+            </main>
+            <Footer/>
+        </div>
+    )
+}
+
+export default MarketingLayout;
