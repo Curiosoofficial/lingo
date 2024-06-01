9231d93c93bc405a3737cd6fae474f2aab4ae498
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-01 22:32:56 +0200

    footer flags

--- app/(marketing)/Footer.tsx
+++ app/(marketing)/Footer.tsx
@@ -1,10 +1,67 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-        Footer
-    </div>
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+        <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image 
+              src="/hr.svg" 
+              alt="croatian flag" 
+              width={32} 
+              height={40} 
+              className="mr-4 rounded-md"
+            />
+            Croatian
+          </Button>
+
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image 
+              src="/es.svg" 
+              alt="spanish flag" 
+              width={32} 
+              height={40} 
+              className="mr-4 rounded-md"
+            />
+            Spanish
+          </Button>
+
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image 
+              src="/fr.svg" 
+              alt="french flag" 
+              width={32} 
+              height={40} 
+              className="mr-4 rounded-md"
+            />
+            French
+          </Button>
+
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image 
+              src="/it.svg" 
+              alt="italian flag" 
+              width={32} 
+              height={40} 
+              className="mr-4 rounded-md"
+            />
+            Italian
+          </Button>
+
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image 
+              src="/jp.svg" 
+              alt="japanese flag" 
+              width={32} 
+              height={40} 
+              className="mr-4 rounded-md"
+            />
+            Japanese
+          </Button>
+        </div>
+    </footer>
   )
 }
 
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, Practice and Master new languages with Lingo
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px]">
           <ClerkLoading>
             <Loader className="size-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
@@ -36,7 +36,7 @@ export default function Home() {
               </SignUpButton>
 
               <SignInButton mode="redirect" forceRedirectUrl="/learn">
-                <Button size="lg" variant="primaryOutline" className="w-full mt-4">
+                <Button size="lg" variant="primaryOutline" className="w-full">
                   I already have an account
                 </Button>
               </SignInButton>
