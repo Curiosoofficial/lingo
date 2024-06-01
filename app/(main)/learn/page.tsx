cbb390322387a86392d660d927b24080048c2df7
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-02 01:53:38 +0200

    page-wrappers implemented

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import FeedWrapper from "@/components/FeedWrapper"
+import StickyWrapper from "@/components/StickyWrapper"
+import Header from "./Header"
+import UserProgress from "@/components/UserProgress"
+import { title } from "process"
+
 
 const page = () => {
   return (
-    <div>learnpage</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress 
+          activeCourse={{title: "Spanish", imageSrc: "/es.svg"}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish"/>
+      </FeedWrapper>
+    </div>
   )
 }
 
