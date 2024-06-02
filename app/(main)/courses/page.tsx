9b69b7f50cc4cb450e96f69ec1cd82a024c38e6d
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-03 01:07:45 +0200

    user progress db

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,10 +1,13 @@
-import { getCourses } from "@/db/queries"
+import { getCourses, getUserProgress } from "@/db/queries"
 
 import List from "./List"
 
 
 const page = async () => {
- const courses = await getCourses()
+ const coursesData = getCourses()
+ const userProgressData = getUserProgress()
+
+ const [courses, userProgress] = await Promise.all([coursesData, userProgressData])
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
@@ -12,7 +15,7 @@ const page = async () => {
         
         <List 
             courses={courses}
-            activeCourseId={1}
+            activeCourseId={userProgress?.activeCourseId}
         />
     </div>
 
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,26 +1,34 @@
-import FeedWrapper from "@/components/FeedWrapper"
-import StickyWrapper from "@/components/StickyWrapper"
-import Header from "./Header"
-import UserProgress from "@/components/UserProgress"
-import { title } from "process"
+import FeedWrapper from "@/components/FeedWrapper";
+import StickyWrapper from "@/components/StickyWrapper";
+import Header from "./Header";
+import UserProgress from "@/components/UserProgress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
 
-const page = () => {
+const page = async () => {
+  const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress 
-          activeCourse={{title: "Spanish", imageSrc: "/es.svg"}}
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
           hearts={5}
           points={100}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Spanish"/>
+        <Header title="Spanish" />
       </FeedWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
