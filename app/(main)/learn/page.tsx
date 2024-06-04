bd18b1a5552a87e825b340cd9f6af0eaca881d59
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-04 21:37:53 +0200

    lesson buttons added

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,14 +2,23 @@ import FeedWrapper from "@/components/FeedWrapper";
 import StickyWrapper from "@/components/StickyWrapper";
 import Header from "./Header";
 import UserProgress from "@/components/UserProgress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
+import Unit from "./Unit";
 
 
 
 const page = async () => {
+  const unitsData = getUnits();
   const userProgressData = getUserProgress();
-  const [userProgress] = await Promise.all([userProgressData]);
+
+  const [
+    userProgress,
+    units
+  ] = await Promise.all([
+    userProgressData,
+    unitsData
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -27,6 +36,19 @@ const page = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
