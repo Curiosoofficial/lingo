8b404793e048cddb64cd02497a7438f1ece1e21d
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-10 00:44:39 +0200

    implemented shop page / heart refill action

--- actions/userProgress.action.ts
+++ actions/userProgress.action.ts
@@ -110,3 +110,31 @@ export const reduceHearts = async (challengeId: number) => {
   revalidatePath(`/learn/${lessonId}`);
 
 }
+
+const POINTS_TO_REFILL = 10;
+
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress();
+
+  if(!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  if(currentUserProgress.hearts === 5) {
+    throw new Error("Hearts are full");
+  }
+  
+  if(currentUserProgress.points < POINTS_TO_REFILL) {
+    throw new Error("Not enough points");
+  }
+
+  await db.update(userProgress).set({
+    hearts: 5,
+    points: currentUserProgress.points - POINTS_TO_REFILL,
+  }).where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+}
